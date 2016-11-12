32cd4bee9ad6c7c66f450bb363d0ed23c96edb2a
hejiafu <hejiafu@secoo.com>
2016-11-12 13:59:43 +0800

    first

---
+++ 11/gp.py
@@ -0,0 +1,61 @@
+#coding:utf-8
+from random import random,randint,choice
+from copy import deepcopy
+from math import log
+
+class fwrapper:
+	def __init__(self, function, childcount, name):
+		self.function = function
+		self.childcount = childcount
+		self.name = name
+		
+class node:
+	def __init__(self,fw,children):
+		self.function = fw.function
+		self.name = fw.name
+		self.children = children
+		
+	def evaluate(self, inp):
+		results = [n.evaluate(inp) for n in self.children]
+		return self.function(results)
+		
+class paramnode:
+	def __init__(self, idx):
+		self.idx = idx
+		
+	def evaluate(self, inp):
+		return inp[self.idx]
+
+class constnode:
+	def __init__(self, v):
+		self.v = v
+		
+	def evaluate(self, inp):
+		return self.v
+		
+addw = fwrapper(lambda l:l[0] + l[1], 2, 'add')
+subw = fwrapper(lambda l:l[0] - l[1], 2, 'subtract')
+mulw = fwrapper(lambda l:l[0] * l[1], 2, 'multiply')
+
+def iffunc(l):
+	if l[0] > 0: return l[1]
+	else: return l[2]
+ifw = fwrapper(iffunc, 3, 'if')
+
+def isgreater(l):
+	if l[0] > l[1]: return 1
+	else: return 0
+gtw = fwrapper(isgreater, 2, 'isgreater')
+
+flist = [addw, mulw, ifw, gtw, subw]
+
+def exampletree():
+	return node(ifw, [
+		node(gtw, [paramnode(0), constnode(3)]),
+		node(addw, [paramnode(1), constnode(5)]),
+		node(subw, [paramnode(1), constnode(2)]),
+		])
+		
+#main
+#exampletree  = exampletree()
+#exampletree.evaluate([2,3])
